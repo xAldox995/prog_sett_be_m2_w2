b7faeca528b2ef60e50130564db947914242e2e4
xAldox995 <valzanialdo@gmail.com>
2024-11-01 19:16:25 +0100

    Initial commit

---
+++ src/main/java/aldovalzani/prog_sett_be_m2_w2/ProgSettBeM2W2Application.java
@@ -0,0 +1,13 @@
+package aldovalzani.prog_sett_be_m2_w2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgSettBeM2W2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgSettBeM2W2Application.class, args);
+	}
+
+}
---
+++ src/test/java/aldovalzani/prog_sett_be_m2_w2/ProgSettBeM2W2ApplicationTests.java
@@ -0,0 +1,13 @@
+package aldovalzani.prog_sett_be_m2_w2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgSettBeM2W2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
