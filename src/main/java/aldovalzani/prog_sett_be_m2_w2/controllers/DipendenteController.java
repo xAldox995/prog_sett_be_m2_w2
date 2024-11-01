a0e6f4b3e9ac58a80c661224cb89c03e28698175
xAldox995 <valzanialdo@gmail.com>
2024-11-01 22:11:27 +0100

    test post

--- src/main/java/aldovalzani/prog_sett_be_m2_w2/controllers/DipendenteController.java
+++ src/main/java/aldovalzani/prog_sett_be_m2_w2/controllers/DipendenteController.java
@@ -2,24 +2,34 @@ package aldovalzani.prog_sett_be_m2_w2.controllers;
 
 import aldovalzani.prog_sett_be_m2_w2.dto.NewDipendenteDTO;
 import aldovalzani.prog_sett_be_m2_w2.entities.Dipendente;
+import aldovalzani.prog_sett_be_m2_w2.exceptions.BadRequestException;
 import aldovalzani.prog_sett_be_m2_w2.services.DipendenteServ;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.stream.Collectors;
+
+/*
+    http://localhost:3001/dipendenti
+ */
 @RestController
+@RequestMapping("/dipendenti")
 public class DipendenteController {
     @Autowired
     private DipendenteServ dipendenteServ;
-    /*
-        http://localhost:3001/dipendenti
-     */
+
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Dipendente saveDipendente (@RequestBody@Validated NewDipendenteDTO body, BindingResult validationResult)
+    public Dipendente saveDipendente(@RequestBody @Validated NewDipendenteDTO body, BindingResult validationResult) {
+        if (validationResult.hasErrors()) {
+            String message = validationResult.getAllErrors().stream().map(objectError -> objectError.getDefaultMessage())
+                    .collect(Collectors.joining(" ,"));
+            throw new BadRequestException("Ci sono errori nel payload! " + message);
+        }
+        return this.dipendenteServ.saveDipendente(body);
+    }
 }
